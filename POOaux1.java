3a550071ce8ce28796f10ada5ee89af7aef61470
Muriloferbhs <murfeg@gmail.com>
2025-05-08 08:37:25 -0300

    Create POOaux1.java

---
+++ POOaux1.java
@@ -0,0 +1,21 @@
+\package com.mycompany.orientadaaobjeto;
+
+public class Retangulo {
+    
+    //atributo
+    
+    public int base;
+    public int altura;
+    
+    //metodos
+    
+    public int area(){
+        
+        return base * altura;
+        
+    }
+    public int perimetro(){
+        return 2 * base + 2 * altura;   
+} 
+
+}
